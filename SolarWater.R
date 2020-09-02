6e394965ab92709efcb27ffcf5011b9dba8bd1fb
sakthipandi <sokey12@gmail.com>
2020-09-02 18:39:58 -0500

    Test

---
+++ SolarWater.R
@@ -0,0 +1,24 @@
+library(shinycssloaders)
+library(shinyjs)
+library(shinyWidgets)
+# hello
+
+ui <- fluidPage(
+  
+  setBackgroundImage(src = "poop.jpeg"),
+  
+  sidebarLayout(sidebarPanel(
+    numericInput(inputId = "scNum", label = "Enter number of solar cells: ", value = 0, min = 0, step = 1),
+    numericInput(inputId = "scPower", label = "Enter solar cell voltage: ", value = 0, min = 0),
+    numericInput(inputId = "Temp", label = "Temperature (°C): ", value = 25, step = 1, min = 0, max = 50)
+  ),
+  
+  mainPanel(
+    
+  )
+  )
+)
+
+Server <- function(input, output) {}
+
+shinyApp(ui=ui, server = server)
