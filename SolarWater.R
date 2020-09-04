4930e67408bd3b2951242d0aac3b8e81c8b5a591
sakthipandi <sokey12@gmail.com>
2020-09-04 13:59:57 -0500

    Now app works. Deleted duplicate

--- SolarWater.R
+++ SolarWater.R
@@ -19,31 +19,6 @@ ui <- fluidPage(
   )
 )
 
-Server <- function(input, output) {}
-
-shinyApp(ui=ui, server = server)
-=======
-library(shinycssloaders)
-library(shinyjs)
-library(shinyWidgets)
-# hello
-
-ui <- fluidPage(
-  
-  setBackgroundImage(src = "poop.jpeg"),
-  
-  sidebarLayout(sidebarPanel(
-    numericInput(inputId = "scNum", label = "Enter number of solar cells: ", value = 0, min = 0, step = 1),
-    numericInput(inputId = "scPower", label = "Enter solar cell voltage: ", value = 0, min = 0),
-    numericInput(inputId = "Temp", label = "Temperature (°C): ", value = 25, step = 1, min = 0, max = 50)
-  ),
-  
-  mainPanel(
-    
-  )
-  )
-)
-
-Server <- function(input, output) {}
+server <- function(input, output) {}
 
 shinyApp(ui=ui, server = server)
