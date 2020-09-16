a678449eae105e3f0ae753e4be3d00bd7cdbbced
sakthipandi <sokey12@gmail.com>
2020-09-16 17:17:10 -0500

    I changed the units from volts to watts.

--- Functions/energyGraph.R
+++ Functions/energyGraph.R
@@ -5,7 +5,8 @@ energyGraph <- function(data, temp){
     geom_line(mapping = aes(x = timeVals, y = energyVals), color = "blue", size = 1.25) +
     labs(title = "Energy Reserves", subtitle = paste("Temperature: ", temp, "°C"),
       x = "Time (hours)",
-      y = "Energy (V)")
+      y = "Energy (W)")
   return (eGraph)
 }
+
 
